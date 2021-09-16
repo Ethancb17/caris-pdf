6133e445ba77299c1378d978c478a8e23d96f8ed
Ethan Cordero <ethan.cb.96@gmail.com>
2021-09-16 16:12:28 -0600

    first commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/preview', function(req, res, next) {
+  res.render('kyc_fisico-OLD', { name: 'Ethan' ,lastname: 'Cordero' });
+});
+
+module.exports = router;
